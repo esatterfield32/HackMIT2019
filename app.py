5e611937f9e4a9e12566555c1d4143dd82f47d92
AmeliaM <ameliameles@gmail.com>
2019-09-14 17:55:42 -0400

    added start speaking

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':  #this block is only entered when the form is submitted
         userInput = request.form['userInput']
+        #alter text
         return render_template('index.html', hasInput = True, userInput = userInput)
+    #if startMicrophone == True:
+
     return render_template('index.html', hasInput = False)
-    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
