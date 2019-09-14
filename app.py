77fa0adb0156297924f7a76265ba05ea7f506eb5
AmeliaM <ameliameles@gmail.com>
2019-09-14 17:15:40 -0400

    integrated flask with html form

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        userInput = request.form['userInput']
+        return render_template('index.html', hasInput = True, userInput = userInput)
+    return render_template('index.html', hasInput = False)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
